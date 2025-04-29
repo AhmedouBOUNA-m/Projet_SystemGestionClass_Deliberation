fc724e54e5f74ef27185d961b381e3a62e5dd36a
AhmedouBOUNA-m <ahmedou@groupeisi.com>
2025-04-29 12:00:34 +0000

    4em cours

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,18 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
-    //initialisation de la base donnees simule
-    let user = JSON.parse(localStorage.getItem("User"))||[];
-    //Getion d'inscription 
-    document.getElementById("formInscription").addEventListener("submit",function (e) {
+document.addEventListener("DOMContentLoaded", () =>{
+    //initialisation de la base donnée simulé
+    let user = JSON.parse(localStorage.getItem("users"))||[];
+    //Gestion d'inscription
+    document.getElementById("formInscrip").addEventListener("submit",function(e){
         e.preventDefault();
         //recuperation des champs
-        const UserName = document.getElementById("username");
-        const UserMail = document.getElementById("mail");
-        const password = document.getElementById("mdp");
-        // console.log(UserName,UserMail,password);
-        const newUser = {UserName,UserMail,password};
-        user.push(newUser);
-        localStorage.setItem(User,JSON.stringify)(user);
-        // alert(Inscription Reussite);
-        
+        const userName = document.getElementById("userName").value;
+        const email = document.getElementById("mail").value;
+        const motDePasse = document.getElementById("mdp").value;
+        // console.log(userName, email, motDePasse);
+        const newUser = {userName,email,motDePasse}
+        user.push(newUser)
+        localStorage.setItem("User", JSON.stringify(user));
+        // alert("Inscruption Reussite")
     })
-});
+
+    // connexion
+    
+    document.getElementById("formconnexion").addEventListener("submit",function (e){
+        e.preventDefault();
+        const email = document.getElementById("Email").value;
+        const motDePasse = document.getElementById("psw").value;
+
+        const userFund = user.find((u) => u.email === email && u.motDePasse === motDePasse);
+        if (userFund) {
+            alert("connexion reussite");
+            // window.location.herf = "index.html";
+        }else{
+            
+            alert("Email ou mot de passe incorrect");
+        }
+    })
+})
